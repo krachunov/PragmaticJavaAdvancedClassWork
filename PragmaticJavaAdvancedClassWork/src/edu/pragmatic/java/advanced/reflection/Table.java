7ab1e57cca968f888036ff2042b6dcd35d95f932
JAVA <JAVA@192.168.0.129>
2015-11-24 21:54:14 +0200

    First Lecture

--- PragmaticJavaAdvancedClassWork/src/edu/pragmatic/java/advanced/reflection/Table.java
+++ PragmaticJavaAdvancedClassWork/src/edu/pragmatic/java/advanced/reflection/Table.java
@@ -1,13 +1,11 @@
 package edu.pragmatic.java.advanced.reflection;
 
-
-
 //@Target(value=ElementType.TYPE)
 //@Retention(RetentionPolicy.RUNTIME)
 //TODO instal full version java
 public @interface Table {
-	
-//	String tableName;
 
-	String tableName();
+	// Показва че ако не е задължителен този метод той трябва да върне нещо и за
+	// това се слага default
+	String tableName() default "";
 }
